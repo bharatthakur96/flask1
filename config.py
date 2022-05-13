0e2cb6468d5640b1c2c9b3346cb1249286493675
= <=>
2022-05-13 17:32:47 +0530

    implemented crud

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
-app.config ["SECRET_KEY"]='7db148d3c46796421b7e8d66406ab0dd'
+# app = Flask(__name__)
+# app.config ["SECRET_KEY"]='7db148d3c46796421b7e8d66406ab0dd'
 
