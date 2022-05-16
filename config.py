c841dce0e8278252326d75177d15b71d655faba7
= <=>
2022-05-16 15:28:01 +0530

    updated config file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config(object):
     DEBUG = True
     DEVELOPMENT = True
-    SECRET_KEY = '895fffdd19c34048387252d2c45034beb613dacc'
+    SECRET_KEY = "895fffdd19c34048387252d2c45034beb613dacc"
     DB_HOST = "sqlite:///todo.db"
