7d1762f0dad900c400ad57eef205066a4851917c
= <=>
2022-05-17 13:21:09 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
-class Config(object):
-    DEBUG = True
-    DEVELOPMENT = True
-    SECRET_KEY = '895fffdd19c34048387252d2c45034beb613dacc'
-    DB_HOST = "sqlite:///todo.db"
+import os
+
+class DevelopmentConfig(object):
+    TESTING = os.environ.get("TESTING")
+    DEBUG = os.environ.get("DEBUG")
+    FLASK_ENV = os.environ.get("FLASK_ENV")
+    FLASK_APP = os.environ.get("FLASK_APP")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "SQLALCHEMY_DATABASE_URI"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
